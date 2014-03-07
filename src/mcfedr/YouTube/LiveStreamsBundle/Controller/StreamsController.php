591baa561801b167b563ac44a4ff7f07e9b3130a
Fred Cox <mcfedr@gmail.com>
2014-03-07 08:47:32 +0200

    Add cache header

--- src/mcfedr/YouTube/LiveStreamsBundle/Controller/StreamsController.php
+++ src/mcfedr/YouTube/LiveStreamsBundle/Controller/StreamsController.php
@@ -4,12 +4,16 @@ namespace mcfedr\YouTube\LiveStreamsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class StreamsController extends Controller
 {
     /**
      * @Route("/streams")
+     * @Method({"GET"})
+     * @Cache(expires="1 hour", public=true)
      */
     public function streamsAction()
     {
